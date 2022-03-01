57a5a019d56d5febda6cbed24a4dcb3c9b863cc5
wp1631 <waragonphoosuwan@gmail.com>
2022-03-01 16:55:30 +0700

    styling

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,23 @@ const sequenceflashfinish = new Event('sequential-flash-finish');
 
 const databaseevent = new Event('database');
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 function main() {
     document.addEventListener('consent-submit', EventFunctions.onConsentSubmit);
     document.addEventListener('preparation-start', EventFunctions.onPreparationStart);
